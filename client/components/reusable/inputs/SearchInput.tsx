61a5ef9d0ee105eed03b06e83114e1a51cb335b0
clive-cudi <clivemaina41@gmail.com>
2022-10-05 20:45:37 +0300

    finished-search-input-frontend

--- client/components/reusable/inputs/SearchInput.tsx
+++ client/components/reusable/inputs/SearchInput.tsx
@@ -1,11 +1,15 @@
 import { ChangeEvent, InputHTMLAttributes, DetailedHTMLProps } from "react"
 import styles from "../../../styles/components/reusable/inputs/searchInput.module.scss";
+import { FiSearch } from "react-icons/fi";
 
-interface SearchInput_Props extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {}
+interface SearchInput_Props extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+    iconAlt?: React.ReactNode | JSX.Element
+}
 
-export const SearchInput = ({ onChange }: SearchInput_Props) => {
+export const SearchInput = ({ onChange, iconAlt }: SearchInput_Props) => {
     return (
         <div className={styles.search_input_wrapper}>
+            <span>{iconAlt ?? <FiSearch />}</span>
             <input type="text" name="search_query" id="query" placeholder="Search for anything..." onChange={onChange} />
         </div>
     )
