04cb103d5274d0de69716eccede1f1d30783a76a
clive-cudi <clivemaina41@gmail.com>
2022-10-06 22:38:51 +0300

    added-greetings-banner-homepage

--- client/components/views/homepageTabs/Hometab.tsx
+++ client/components/views/homepageTabs/Hometab.tsx
@@ -1,7 +1,35 @@
+import styles from "../../../styles/views/homePageTabs/hometab.module.scss";
+import Image from "next/image";
+import { RegularBtn } from "../../reusable";
+import { BsPlay } from "react-icons/bs";
+
 export const HomeTab = ({}): JSX.Element => {
     return (
-        <div>
-            
+        <div className={styles.hb_content}>
+            <div className={styles.hb_greetings_wrapper}>
+                <div className={styles.hb_banner_wrapper}>
+                    <div className={styles.hb_banner_info}>
+                        <h2>Good Morning Madaline</h2>
+                        <RegularBtn label="Get Started" className={styles.get_started_btn} withIcon={{status: true, icon: <BsPlay />, orientation: "start"}} />
+                    </div>
+                    <div className={styles.hb_banner_ill}>
+                        <div className={styles.hb_ill}>
+                            <Image src={"/common/Illustration_4.png"} alt="@" layout="fill" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.hb_info_wrapper}>
+                <div className={styles.hb_tasks_wrapper}>
+
+                </div>
+                <div className={styles.hb_calendar_wrapper}>
+
+                </div>
+            </div>
+            <div className={styles.hb_projects_wrapper}>
+
+            </div>
         </div>
     )
 }
