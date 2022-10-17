90d39388e8ca2cc38f7783269fe50747876dfd41
clive-cudi <clivemaina41@gmail.com>
2022-10-17 14:11:27 +0300

    added-settings-account-tab-user-inputs-frontend

--- client/components/views/settings/AccountTab.tsx
+++ client/components/views/settings/AccountTab.tsx
@@ -2,6 +2,7 @@ import styles from "../../../styles/views/settings/accountTab.module.scss";
 import { IconBtn, RegularBtn } from "../../reusable";
 import { VscBell } from "react-icons/vsc";
 import { MdOutlineSecurity } from "react-icons/md";
+import { InputDiv, InputSelect } from "../../reusable";
 
 export const AccountTab = ():JSX.Element => {
     return (
@@ -27,7 +28,81 @@ export const AccountTab = ():JSX.Element => {
                 </div>
             </nav>
             <div className={styles.at_content_view_wrapper}>
+                <div className={styles.at_user_info_wrapper}>
+                    <div className={styles.user_info_strip}>
+                        <div className={styles.user_info_strip_title}>
+                            <h3>User Information</h3>
+                        </div>
+                        <div className={`${styles.user_info_strip_content} ${styles.user_info_strip_grid_cols_0}`}>
+                            <p>
+                                Enter the required information below to register. You can change it anytime you want.
+                            </p>
+                        </div>
+                    </div>
+                    <div className={styles.user_info_strip}>
+                        <div className={styles.user_info_strip_title}>
+                            <h4>Email address</h4>
+                        </div>
+                        <div className={`${styles.user_info_strip_content} ${styles.user_info_strip_grid_cols_0}`}>
+                            <span>
+                                <InputDiv type={`email`} placeholder={`username@domain.com`} onChange={()=>{}} />
+                            </span>
+                        </div>
+                    </div>
+                    <div className={styles.user_info_strip}>
+                        <div className={styles.user_info_strip_title}>
+                            <h4>Full Name</h4>
+                        </div>
+                        <div className={`${styles.user_info_strip_content} ${styles.user_info_strip_grid_cols_2}`}>
+                            <span>
+                                <InputDiv type={`text`} placeholder={"First Name"} onChange={()=>{}} />
+                            </span>
+                            <span>
+                                <InputDiv type={`text`} placeholder={"Second Name"} onChange={()=>{}} />
+                            </span>
+                        </div>
+                    </div>
+                    <div className={styles.user_info_strip}>
+                        <div className={styles.user_info_strip_title}>
+                            <h4>Address</h4>
+                        </div>
+                        <div className={`${styles.user_info_strip_content} ${styles.user_info_strip_grid_cols_3}`}>
+                            <span>
+                                <InputSelect options={[]} />
+                            </span>
+                            <span>
+                                <InputSelect options={[]} />
+                            </span>
+                            <span>
+                                <InputDiv type={`number`} placeholder={`254`} onChange={()=>{}} />
+                            </span>
+                            <span data-span={"full"}>
+                                <InputDiv type={`text`} placeholder={`Street`} onChange={()=>{}} />
+                            </span>
+                        </div>
+                    </div>
+                    <div className={styles.user_info_strip}>
+                        <div className={styles.user_info_strip_title}>
+                            <h4>Role</h4>
+                        </div>
+                        <div className={`${styles.user_info_strip_content} ${styles.user_info_strip_grid_cols_0}`}>
+                            <span>
+                                <InputDiv type={`text`} placeholder={`Role`} onChange={()=>{}} />
+                            </span>
+                        </div>
+                    </div>
+                    <div className={styles.user_info_strip}>
+                        <div className={styles.user_info_strip_title}>
+                            <h4>Choose your skills</h4>
+                        </div>
+                        <div className={`${styles.user_info_strip_content} ${styles.user_info_strip_grid_cols_0}`}>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.at_user_profile_wrapper}>
 
+                </div>
             </div>
         </div>
     )
