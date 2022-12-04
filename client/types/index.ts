fe6507fd69db5bb55896f1d62f9879909dd3b0d9
clive-cudi <clivemaina41@gmail.com>
2022-12-04 20:59:55 +0300

    init-project-store-frontend

--- client/types/index.ts
+++ client/types/index.ts
@@ -2,3 +2,4 @@ export type { API_res_model, Api_User_res } from "./apiRes";
 export type { PageAuth } from "./auth";
 export * from "./general";
 export * from "./tasks";
+export * from "./projects";
