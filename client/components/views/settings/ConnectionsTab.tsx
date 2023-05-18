f9c3b79402cea43b7de0f1469d7b2a49720bd0b0
clive-cudi <clivemaina41@gmail.com>
2023-05-18 22:35:06 +0300

    fixed-build-errors-connections-tab-map-keys-iterator-client

--- client/components/views/settings/ConnectionsTab.tsx
+++ client/components/views/settings/ConnectionsTab.tsx
@@ -6,37 +6,37 @@ export const ConnectionsTab = (): JSX.Element => {
     const [integrations, setIntegrations] = useState([
         {
             label: "Google Calendar",
-            icon: <Image src={"/logos/google-calendar.png"} layout={"fill"} />,
+            icon: <Image src={"/logos/google-calendar.png"} layout={"fill"} alt={"@"} />,
             content: "Create, edit, and share spreadsheets wherever you are with Google Sheets."
         },
         {
             label: "Slack",
-            icon: <Image src={`/logos/slack.png`} layout={"fill"} />,
+            icon: <Image src={`/logos/slack.png`} layout={"fill"} alt={"@"} />,
             content: "Slack is a platform for team communication: everything in one place."
         },
         {
             label: "Google Drive",
-            icon: <Image src={`/logos/google-drive.png`} layout={"fill"} />,
+            icon: <Image src={`/logos/google-drive.png`} layout={"fill"} alt={"@"} />,
             content: "Google Drive is Google's file sync app that lets you store all of your files online."
         },
         {
             label: "Calendly",
-            icon: <Image src={`/logos/calendar.png`} layout={"fill"} />,
+            icon: <Image src={`/logos/calendar.png`} layout={"fill"} alt={"@"} />,
             content: "Calendly is an elegant and simple scheduling tool for businesses that eliminates email back and forth."
         },
         {
             label: "Outlook",
-            icon: <Image src={`/logos/outlook.png`} layout={"fill"} />,
+            icon: <Image src={`/logos/outlook.png`} layout={"fill"} alt={"@"} />,
             content: "Microsoft Outlook is a web-based suite of webmail, contacts, tasks, and calendaring services."
         },
         {
             label: "Github",
-            icon: <Image src={`/logos/github.png`} layout={"fill"} />,
+            icon: <Image src={`/logos/github.png`} layout={"fill"} alt={"@"} />,
             content: "Cloud based distributed version control for software."
         },
         {
             label: "Google Sheets",
-            icon: <Image src={`/logos/google-sheets.png`} layout={"fill"} />,
+            icon: <Image src={`/logos/google-sheets.png`} layout={"fill"} alt={"@"} />,
             content: "Create, edit, and share spreadsheets wherever you are with Google Sheets, and get automated insights from your data."
         },
         {
@@ -62,7 +62,7 @@ export const ConnectionsTab = (): JSX.Element => {
                 <div className={styles.ct_integrations_wrapper}>
                     {integrations.map((integration) => {
                         return (
-                            <div className={styles.ct_int_widget}>
+                            <div key={crypto.randomUUID()} className={styles.ct_int_widget}>
                                 <div className={styles.ct_int_col}>
                                     <span className={styles.ct_int_icon}>{integration.icon}</span>
                                 </div>
